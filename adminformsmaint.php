62419dba02318209465dba3651e10408b66b8db5
klinzmandr <dave.klinzman@yahoo.com>
2014-12-06 10:32:42 -0800

    added ability to rename existing forms

--- adminformsmaint.php
+++ adminformsmaint.php
@@ -11,20 +11,36 @@
 <h1>Forms Directory Maintenance</h1>
 <hr>
 <table cellpadding="0" cellspacing="0" border="0" align="center" width="95%">
-
+<script src="jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
 <?php
 session_start();
+// include 'Incls/vardump.inc';
 include 'Incls/seccheck.inc';
 include 'Incls/mainmenu.inc';
 
 $action = isset($_REQUEST['action'])? $_REQUEST['action'] : "";
 $form =isset($_REQUEST['form'])? $_REQUEST['form'] : ""; 
 
+if ($action == 'rename') {
+	$old = 'Forms/' . $_REQUEST['oldname'];
+	$new = 'Forms/' . $_REQUEST['newname'];
+//	echo 'Rename request received<br>';
+//	echo "oldname: $old, newname: $new<br>";
+	if (rename($old, $new)) { 
+//		echo "Renamed '$old' to '$new'<br>";
+		}
+	else { 
+		echo "Rename request FAILED!<br>
+		New name provided already exists OR path name invalid";
+		}
+	}
+
 if ($action == 'delete') {
-	echo '<h4>Delete action requested.</h4>';	
+//	echo '<h4>Delete action requested.</h4>';	
 	$deltarget = 'Forms/' . $form;
 	unlink($deltarget);
-	echo "<h4>Form $form has been successfully deleted.</h4>";
+	echo "<h4>Form &apos;$form&apos; has been successfully deleted.</h4>";
 	}
 
 if ($action == 'addnew') {
@@ -43,7 +59,7 @@ if ($action == 'addnew') {
       }
     else {
       move_uploaded_file($_FILES["file"]["tmp_name"], "Forms/" . $_FILES["file"]["name"]);
-      echo "<h3>Upload successful. Stored in: " . "Forms/" . $_FILES["file"]["name"] . '</h3>';
+      echo "<h3>Upload successful. File stored as: " . "&apos;" . $_FILES["file"]["name"] . '&apos;</h3>';
       }
     }
   }
@@ -57,24 +73,61 @@ $forms = scandir('Forms');
 // list contents of forms dir
 foreach ($forms as $formname) {
 if (($formname == '.') || ($formname == '..')) { continue; }
-echo "<tr><td width=\"15%\" align=\"center\">
-<a href=\"adminformsmaint.php?action=delete&form=$formname\">Delete</a>
-</td><td><a target=_blank href=\"Forms/$formname\">$formname</a></td></tr>";
+print <<<listPart1
+<tr><td width="15%" align="center">
+<a id="DF" href="adminformsmaint.php?action=delete&form=$formname">Delete</a>
+&nbsp;/&nbsp;
+<a href="#" onclick="return getfld('$formname')">Rename</a>
+</td>
+<td>
+<a target=_blank href="Forms/$formname">$formname</a></td></tr>
+listPart1;
 }
 ?>
-
 </table>
-
-<br>
+<script>
+$( "#DF" ).click(function() {
+	var r = confirm("This action permanently deletes the file.\nThis action CANNOT be reversed.\n\nClick OK to continue.");
+	if (r == true) { return true; } 
+	else { return false; }
+});
+</script>
 <form action="adminformsmaint.php" method="post" enctype="multipart/form-data">
-<label for="file">or add a new one:&nbsp;</label>
+<!-- <label for="file">or add a new one:&nbsp;</label> -->
+<br>OR ADD A NEW ONE
 <input size=50 type="file" name="file" id="file" />
 <input type="hidden" name="action" value="addnew">
 <input type="submit" name="submit" value="Submit" />
 </form>
-<br><br><br>
+
 </div>
-<script src="jquery.js"></script>
-<script src="js/bootstrap.min.js"></script>
+// rename function and scripts
+<script>
+function getfld(OName) {
+var inval = OName;
+//	if prompt dialog is canceled it exits the script
+var val = prompt("Please enter a NEW name (including the file extension if needed):",inval);
+if (val.length > 0) {
+// if confirm dialog is canceled it returns false
+	var r = confirm("Confirm rename of "+inval+" to "+val);
+	if (r == true) {
+		document.getElementById("HF1").value = inval;
+		document.getElementById("HF2").value = val;
+		document.forms["NameForm"].submit();
+  	return true;
+		}
+	}
+alert("Rename action cancelled");
+return false;
+}
+</script>
+
+<!-- define form to submit WITHOUT a submit field defined -->
+<form method="post" name="NameForm">
+<input type="hidden" id="HF1" name="oldname" value="">
+<input type="hidden" id="HF2" name="newname" value="">
+<input type="hidden" name="action" value="rename">
+</form>
+
 </body>
 </html>
